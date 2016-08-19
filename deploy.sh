554b27f46ee205cff1d700925197e20e9f65a57b
Raymund Lew <raymund.lew@gmail.com>
2016-08-18 17:09:49 -0700

    edited deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-# bundle exec jekyll build
+rm -rf _site/
+
+# make sure we can run bundle
+chruby ruby
+bundle exec jekyll build
 # bundle exec travis-lint
 # bundle exec htmlproof ${HTML_FOLDER} --disable-external
 
 # deploy
-git checkout gh-pages
-git subtree split --prefix _site/ -b master
-git push -f origin master:master
-git branch -D master
+cd _site/
+git init
+git add -A
+git commit -m 'deploy to Github Pages'
+git push --force https://github.com/18thletter/18thletter.github.io.git master:master
